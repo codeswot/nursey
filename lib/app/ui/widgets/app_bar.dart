a37fb31e47ae2ec25245eea5a5203d4a811ee063
Mubarak Ibrahim <40618838+codeswot@users.noreply.github.com>
2022-10-16 10:28:48 +0100

    app bar db values

--- lib/app/ui/widgets/app_bar.dart
+++ lib/app/ui/widgets/app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nursey/app/bloc/nurse/nurse_bloc.dart';
+import 'package:nursey/app/ui/screens/task/task_detail.dart';
 import 'package:nursey/app/ui/widgets/app_bg.dart';
 import 'package:nursey/app/utils/extensions/build_context_extension.dart';
 import 'package:nursey/configs/configs.dart';
@@ -11,106 +13,118 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       : super(key: key);
   final bool isHome;
   final String? title;
+
   @override
   Widget build(BuildContext context) {
     return AppBg(
       pattern: AppBgPatterns.bgPattern1,
-      child: Container(
-        padding:
-            const EdgeInsets.only(left: 16, right: 16, top: 19, bottom: 10),
-        decoration: BoxDecoration(
-          color: AppColors.primaryAccent.withOpacity(0.2),
-          borderRadius: const BorderRadius.only(
-            bottomRight: Radius.circular(50),
-          ),
-        ),
-        child: SafeArea(
-          child: isHome
-              ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
+      child: BlocBuilder<NurseBloc, NurseState>(
+        builder: (context, state) {
+          return Container(
+            padding:
+                const EdgeInsets.only(left: 16, right: 16, top: 19, bottom: 10),
+            decoration: BoxDecoration(
+              color: AppColors.primaryAccent.withOpacity(0.2),
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+              ),
+            ),
+            child: SafeArea(
+              child: isHome
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Column(
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              'Hi, Mr Musa 👋',
-                              style: GoogleFonts.nunito(
-                                fontWeight: FontWeight.bold,
-                                color: AppColors.secondaryText,
-                                fontSize: 30,
-                              ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Hi, ${state.nurse?.name} 👋',
+                                  style: GoogleFonts.nunito(
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors.secondaryText,
+                                    fontSize: 30,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                ShiftWidget(
+                                  state.nurse?.shift ?? '',
+                                  isCompleting: true,
+                                ),
+                                const SizedBox(height: 15),
+                                Text(
+                                  '10 Tasks Pending',
+                                  style: GoogleFonts.nunito(
+                                    color: AppColors.secondaryText2,
+                                    fontSize: 19,
+                                  ),
+                                ),
+                              ],
                             ),
-                            const SizedBox(height: 5),
-                            Text(
-                              'Morning Shift',
-                              style: GoogleFonts.nunito(
-                                color: AppColors.primaryError,
-                                fontSize: 14,
-                              ),
-                            ),
-                            const SizedBox(height: 15),
-                            Text(
-                              '10 Tasks Pending',
-                              style: GoogleFonts.nunito(
-                                color: AppColors.secondaryText2,
-                                fontSize: 19,
+                            Spring.bubbleButton(
+                              child: CircleAvatar(
+                                radius: 37,
+                                backgroundColor:
+                                    AppColors.primaryAccent.withOpacity(0.5),
+                                child: const CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: AppColors.secondaryAccent,
+                                  backgroundImage: NetworkImage(
+                                      'https://www.shareicon.net/data/128x128/2016/07/05/791390_people_512x512.png'),
+                                ),
                               ),
                             ),
                           ],
                         ),
+                        const TaskFilterPicker(),
+                      ],
+                    )
+                  : Row(
+                      children: [
                         Spring.bubbleButton(
-                          child: CircleAvatar(
-                            radius: 37,
-                            backgroundColor:
-                                AppColors.primaryAccent.withOpacity(0.5),
-                            child: const CircleAvatar(
-                              radius: 30,
-                              backgroundColor: AppColors.secondaryAccent,
-                              backgroundImage: NetworkImage(
-                                  'https://www.shareicon.net/data/128x128/2016/07/05/791390_people_512x512.png'),
+                          child: InkWell(
+                            onTap: () => context.pop(),
+                            child: const Icon(
+                              Icons.arrow_back_ios,
+                              color: AppColors.secondaryText,
+                              size: 30,
                             ),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Text(
+                          title ?? '',
+                          style: GoogleFonts.nunito(
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.secondaryText,
+                            fontSize: 30,
                           ),
                         ),
                       ],
                     ),
-                    const TaskFilterPicker(),
-                  ],
-                )
-              : Row(
-                  children: [
-                    Spring.bubbleButton(
-                      child: InkWell(
-                        onTap: () => context.pop(),
-                        child: const Icon(
-                          Icons.arrow_back_ios,
-                          color: AppColors.secondaryText,
-                          size: 30,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Text(
-                      title ?? '',
-                      style: GoogleFonts.nunito(
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.secondaryText,
-                        fontSize: 30,
-                      ),
-                    ),
-                  ],
-                ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
 
   @override
   Size get preferredSize => const Size(double.infinity, 200);
+}
+
+//TODO : CREATE A TASK FILTER, by status and count pending tasks
+class TaskCount extends StatelessWidget {
+  const TaskCount({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
 
 class TaskFilterPicker extends StatefulWidget {
@@ -122,6 +136,7 @@ class TaskFilterPicker extends StatefulWidget {
 
 class _TaskFilterPickerState extends State<TaskFilterPicker> {
   int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Row(
